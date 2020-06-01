c073682d5116e9b98ebaa3387a07de23f3c430fe
Shreya <shreyabarua2@gmail.com>
2020-06-01 18:39:10 -0400

    completing servlets walkthrough

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,7 +26,8 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String message = "Hello Shreya!";
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Shreya!</h1>");
+    response.getWriter().println(message);
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -29,3 +29,45 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
+
+/**
+ * Fetches a message from the server and adds it to the page.
+ */
+function getMessage() {
+  console.log('Fetching the message');
+
+  // The fetch() function returns a Promise because the request is asynchronous.
+  const responsePromise = fetch('/data');
+
+  // When the request is complete, pass the response into handleResponse().
+  responsePromise.then(handleResponse);
+}
+
+/**
+ * Handles response by converting it to text and passing the result to
+ * addQuoteToDom().
+ */
+function handleResponse(response) {
+  console.log('Handling the response.');
+
+  // response.text() returns a Promise, because the response is a stream of
+  // content and not a simple variable.
+  const textPromise = response.text();
+
+  // When the response is converted to text, pass the result into the
+  // addMsgToDom() function.
+  textPromise.then(addMsgToDom);
+}
+
+/** Adds message to the DOM. */
+function addMsgToDom(message) {
+  console.log('Adding message to dom: ' + message);
+  const msgContainer = document.getElementById('msg-container');
+  msgContainer.innerText = message;
+}
+
+async function getMsgUsingAsyncAwait() {
+  const response = await fetch('/data');
+  const msg = await response.text();
+  document.getElementById('msg-container').innerText = msg;
+}
