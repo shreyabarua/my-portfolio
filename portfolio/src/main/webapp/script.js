fec83dea2ce02b2f4589d1dfedf528f45a4fbeea
Shreya <shreyabarua2@gmail.com>
2020-06-02 15:08:43 +0000

    making functions more concise

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -30,40 +30,19 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
 }
 
+function getRandomQuoteUsingArrowFunctions() {
+  fetch('/random-quote').then(response => response.text()).then((quote) => {
+    document.getElementById('quote-container').innerText = quote;
+  });
+}
+
 /**
  * Fetches a message from the server and adds it to the page.
  */
 function getMessage() {
-  console.log('Fetching the message');
-
-  // The fetch() function returns a Promise because the request is asynchronous.
-  const responsePromise = fetch('/data');
-
-  // When the request is complete, pass the response into handleResponse().
-  responsePromise.then(handleResponse);
-}
-
-/**
- * Handles response by converting it to text and passing the result to
- * addQuoteToDom().
- */
-function handleResponse(response) {
-  console.log('Handling the response.');
-
-  // response.text() returns a Promise, because the response is a stream of
-  // content and not a simple variable.
-  const textPromise = response.text();
-
-  // When the response is converted to text, pass the result into the
-  // addMsgToDom() function.
-  textPromise.then(addMsgToDom);
-}
-
-/** Adds message to the DOM. */
-function addMsgToDom(message) {
-  console.log('Adding message to dom: ' + message);
-  const msgContainer = document.getElementById('msg-container');
-  msgContainer.innerText = message;
+    fetch('/data').then(response => response.text()).then((message) => {
+        document.getElementById('msg-container').innerText = message;
+    });
 }
 
 async function getMsgUsingAsyncAwait() {
