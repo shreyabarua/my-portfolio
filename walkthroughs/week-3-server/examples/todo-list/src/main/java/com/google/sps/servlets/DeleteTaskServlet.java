eaa7e5b3c1c9fcd6cee74c542ce27073abfa3d72
Shreya <shreyabarua2@gmail.com>
2020-06-04 20:54:03 +0000

    making everything old again

--- walkthroughs/week-3-server/examples/todo-list/src/main/java/com/google/sps/servlets/DeleteTaskServlet.java
+++ walkthroughs/week-3-server/examples/todo-list/src/main/java/com/google/sps/servlets/DeleteTaskServlet.java
@@ -27,13 +27,13 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet responsible for deleting tasks. */
 @WebServlet("/delete-task")
 public class DeleteTaskServlet extends HttpServlet {
-
+/*
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     long id = Long.parseLong(request.getParameter("id"));
-
     Key taskEntityKey = KeyFactory.createKey("Task", id);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.delete(taskEntityKey);
   }
+*/
 }
